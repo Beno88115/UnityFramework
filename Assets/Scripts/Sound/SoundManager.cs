cbbee1bb2fc4b9c6731556b87504942fbe88cfe7
beno <benocnn@gmail.com>
2019-10-21 09:46:33 +0800

    fix: xxx

--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -2,18 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using GameFramework;
+using GameFramework.Sound;
+using GameFramework.Resource;
 
 public class SoundManager : SingletonMono<SoundManager> 
 {
-    private GameFramework.Sound.ISoundModule m_SoundModule;
+    private ISoundModule m_SoundModule;
 
     protected override void Awake()
     {
         base.Awake();
 
-        this.m_SoundModule = GameFrameworkEntry.GetModule<GameFramework.Sound.ISoundModule>();
+        this.m_SoundModule = GameFrameworkEntry.GetModule<ISoundModule>();
         this.m_SoundModule.SetSoundHelper(new SoundHelper());
-        this.m_SoundModule.SetResourceModule(GameFrameworkEntry.GetModule<GameFramework.Resource.IResourceModule>());
+        this.m_SoundModule.SetResourceModule(GameFrameworkEntry.GetModule<IResourceModule>());
     }
 
     private void Start()
